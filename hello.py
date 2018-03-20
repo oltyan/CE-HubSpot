bbc68973edd0901a91afa3336f049750aa5cee70
Chris Oltyan <oltyan@gmail.com>
2018-03-19 23:38:12 -0600

    Cleanup of hello.py

--- hello.py
+++ hello.py
@@ -9,10 +9,4 @@ apikey = "demo"
 def hello():
     payload = {'hapikey' : 'demo'}
     r = requests.get('https://api.hubapi.com/contacts/v1/lists/all/contacts/all', params=payload)
-    prepared = print(r.json)
     return r.text
-
-def hubspot():
-    payload = {'hapikey' : 'demo'}
-    r = requests.get('https://api.hubapi.com/contacts/v1/lists/all/contacts/all', params=payload)
-    print(r)
