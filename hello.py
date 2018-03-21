bb2cdbd00043e29d5b61307cc5bb0f0ab827eacb
Chris Oltyan <oltyan@gmail.com>
2018-03-20 23:30:58 -0600

    Updated hello.py, added /recent route, created a bunch of text cases in main.py, and generall fooled around with API some

--- hello.py
+++ hello.py
@@ -9,4 +9,15 @@ apikey = "demo"
 def hello():
     payload = {'hapikey' : 'demo'}
     r = requests.get('https://api.hubapi.com/contacts/v1/lists/all/contacts/all', params=payload)
-    return r.text
+    total_contacts = r.headers.get('Content-Length')
+    print(total_contacts)
+    display_text = "Total Contacts in System %s" % total_contacts
+    return display_text
+
+@app.route("/recent")
+def recent_contacts():
+    payload = {'hapikey' : 'demo'}
+    recent_contacts = requests.get('https://api.hubapi.com/contacts/v1/lists/recently_updated/contacts/recent',
+                                   params=payload)
+    display_text = recent_contacts.text
+    return display_text
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import requests
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+payload = {'hapikey': 'demo'}
+r = requests.get('https://api.hubapi.com/contacts/v1/lists/all/contacts/all', params=payload)
+print(r.json)
+print(r.headers)
+#print(r.text)
+#for key, value in r.headers.items():
+#    print(key, value)
+#print(r.headers)
+total_contacts = r.headers.get('Content-Length')
+print(total_contacts)
+print(r.json)
+recent_contacts = requests.get('https://api.hubapi.com/contacts/v1/lists/recently_updated/contacts/recent',
+                               params=payload)
+
+
